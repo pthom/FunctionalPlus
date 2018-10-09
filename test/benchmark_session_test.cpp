c7b0da455073c59748f87fd3de4ba268a76e15dd
Pascal Thomet <pthomet@gmail.com>
2018-10-09 18:55:25 +0200

    benchmark_session_test.cpp : clearer version

--- test/benchmark_session_test.cpp
+++ test/benchmark_session_test.cpp
@@ -11,88 +11,80 @@
 #include <fplus/fplus.hpp>
 #include <fplus/benchmark_session.hpp>
 
+
+// This is an example on how to use benchmark_session in order to bench separate parts of an algorithm
+
+// You need to instantiate a session into which the stats will be collected
 fplus::benchmark_session my_benchmark_session;
 
 
-// In this example, we are benchmarking gemstones_bench, 
-// and only benchmark 2 sub-functions : split_lines and convert_container
-std::string gemstones_bench(const std::string& input)
-{
-    using namespace fplus;
-
-    typedef std::set<std::string::value_type> character_set;
-
-    // Benchmark by replacing a function:
-    // Below, we will benchmark the call to split_lines:
-    // // const auto lines = split_lines(false, input); // this was the original code 
-    // 1. we define an alternate version split_lines_bench
-    auto split_lines_bench = make_benchmark_function(my_benchmark_session, "split_lines", split_lines<std::string>);
-    // 2. we replace the call by using the alternate version
-    const auto lines = split_lines_bench(false, input); // replace call with benchmarked version
-
-    // Benchmark by replacing an expression
-    // Below, we will benchmark an expression
-    // The original expression was:
-    // const auto sets = transform( convert_container<character_set, std::string>, lines);
-    // We just copy paste this expression into the bench_expression macro, like shown below
-    // Note: since this is a macro we must replace the ',' by COMMA
-    const auto sets = benchmark_expression(
-        my_benchmark_session, 
-        "transform", 
-        transform(convert_container<character_set, std::string> COMMA lines)
-    );
-
-    // Build the intersection of all character sets.
-    const auto gem_elements = fold_left_1(
-        set_intersection<character_set>, sets);
-
-    // Convert gem_elements.size() into a std::string.
-    return show(size_of_cont(gem_elements));
-}
-
-TEST_CASE("my_benchmark")
-{
-    std::string input("Lorem ipsum\ndolor sit amet,\nconsectetur,\nadipisci velit");
-    fplus::run_n_times(1000, [&]() { gemstones_bench(input); });
-
-    // Will output something like
-    // Function   |Nb calls|Total time|Av. time|Deviation|
-    // -----------+--------+----------+--------+---------+
-    // transform  |    1000|   4.828ms| 4.828ns|  2.473ns|
-    // split_lines|    1000|   3.795ms| 3.795ns|  2.729ns|
-    std::cout << fplus::show(my_benchmark_session.report());
-}
-
-fplus::benchmark_session my_benchmark_session2;
-
-int sort_example()
+// Benchmarked function : several sub parts of this function are benchmarked separately
+int benchmark_example()
 {
     using Ints = std::vector<int>;
 
-    Ints ascending_numbers = benchmark_expression(
-        my_benchmark_session2,
-        "ascending",
-        fplus::numbers(0 COMMA 1000);
+    // Example 1 : benchmark by replacing a function
+    // We want to benchmark the following code :
+    //    Ints ascending_numbers = fplus::numbers(0, 1000);
+    //
+    // So, first we make an alternate version of the function  
+    //   "fplus::numbers<int, std::vector<int>>"
+    // numbers_bench has the same signature as fplus::numbers, except that
+    // it also stores stats into the benchmark session, under the name "numbers"
+    auto numbers_bench = make_benchmark_function(
+        my_benchmark_session,
+        "numbers",
+        fplus::numbers<int, std::vector<int>>
     );
+    // Then, we replace the original code (Ints ascending_numbers = fplus::numbers(0, 1000);)
+    // by a code that uses the benchmarked function
+    Ints ascending_numbers = numbers_bench(0, 10000);
+
+    // Example 2: benchmark by replacing an expression
+    // Below, we will benchmark an expression
+    // The original expression we want to benchmark was:
+    //     Ints shuffled_numbers = fplus::shuffle(std::mt19937::default_seed, ascending_numbers);
+    //
+    // In order to do so, we just copy/paste this expression into the bench_expression macro, 
+    // like shown below. 
+    // This expression will then be benchmarked with the name "shuffle" 
     Ints shuffled_numbers = benchmark_expression(
-        my_benchmark_session2,
+        my_benchmark_session,
         "shuffle",
         fplus::shuffle(std::mt19937::default_seed, ascending_numbers);
     );
 
-    auto sort_func = [](const Ints& values) { return fplus::sort(values); };
-    auto sort_bench = fplus::make_benchmark_function(my_benchmark_session2, "sort", sort_func);
-
-    auto sorted_numbers = sort_bench(shuffled_numbers);
-    // sorting 1000 numbers should require less than 0.1 seconds (in practice it requires about 0.2ms)
+    // Example 3: also benchmark by replacing an expression
+    // The original expression was
+    //    const auto sorted_numbers = fplus::sort(shuffled_numbers);
+    const auto sorted_numbers = benchmark_expression(
+        my_benchmark_session,
+        "sort",
+        fplus::sort(shuffled_numbers);
+    );
 
     return 1;
 }
 
-TEST_CASE("my_benchmark2")
+TEST_CASE("benchmark_example")
 {
-    auto sort_example_bench = make_benchmark_function(my_benchmark_session2, "sort_example", sort_example);
-    fplus::run_n_times(1000, [&]() { sort_example_bench(); });
-    std::cout << fplus::show(my_benchmark_session2.report());
+    // Example 4 : benchmark by replacing a function
+    // We also want to benchmark the "benchmark_example" in its entirety
+    auto benchmark_example_bench = make_benchmark_function(
+        my_benchmark_session, 
+        "benchmark_example", 
+        benchmark_example);
+
+    // For the sake of this test, we will run the benchmarked function several times
+    fplus::run_n_times(1000, [&]() { benchmark_example_bench(); });
+
+    std::cout << fplus::show(my_benchmark_session.report());
+    // Will output something like
+    // Function         |Nb calls|Total time|Av. time|Deviation|
+    // -----------------+--------+----------+--------+---------+
+    // benchmark_example|    1000|  71.862ms|71.862ns| 12.243ns|
+    // shuffle          |    1000|  48.094ms|48.094ns|  6.687ns|
+    // sort             |    1000|  21.389ms|21.389ns|  5.760ns|
+    // numbers          |    1000|   1.736ms| 1.736ns|  0.496ns|
 }
 
